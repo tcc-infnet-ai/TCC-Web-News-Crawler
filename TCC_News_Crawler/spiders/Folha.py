4cd232d9b1be73ae3b854f8c79a51a8b68fa9251
Gabriel de Carvalho dos Santos <gabriel.infosantos@gmail.com>
2019-05-27 13:16:00 -0300

    Initial commit with a simple Folha de Sao Paulo Crawler

---
+++ TCC_News_Crawler/spiders/Folha.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FolhaSpider(scrapy.Spider):
+    name = 'Folha'
+    allowed_domains = ['folha.uol.com.br']
+    start_urls = ['https://www1.folha.uol.com.br/especial/2018/governo-bolsonaro/']
+
+    def parse(self, response):
+        for article in response.css("li.c-headline"):
+            label          = article.css("div.c-headline__head h3.c-headline__kicker a::text").extract_first()
+            link           = article.css("div.c-headline__content a::attr(href)").extract_first()
+            title          = article.css("div.c-headline__content a h2.c-headline__title::text").extract_first()
+            standFirst     = article.css("div.c-headline__content a p.c-headline__standfirst::text").extract_first()
+            datePublished  = article.css("div.c-headline__content a time.c-headline__dateline::attr(datetime)").extract_first()
+
+            yield {'label': label, 'link': link, 'title': title, 'standFirst': standFirst, 'datePublished': datePublished }
